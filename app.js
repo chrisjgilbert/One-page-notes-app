8da96fc178b120fef4e9ea4908e66d5da870da85
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-12 17:28:11 +0000

    app.js wrapped in self IIFE

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-window.addEventListener("load", function() {
+"use strict";
 
-  var app = "app";
-  var element = "submit";
+(function() {
+  window.addEventListener("load", function() {
 
-  var note = Note;
-  var notesView = new NotesView();
+      var app = "app";
+      var element = "submit";
 
-  var controller = new NotesController(note, notesView, element, app);
+      var controller = new NotesController(
+        Note,
+        new NotesView,
+        element,
+        app
+      );
 
-  controller.updateDOM();
-})
+      controller.updateDOM();
+    })
+})();
